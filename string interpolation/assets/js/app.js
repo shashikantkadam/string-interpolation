3caca31c129acf1bfff81d61785d64f5670930df
shashikantkadam <shashikantkadam261298@gmail.com>
2022-04-03 07:44:34 +0530

    string interpolation

---
+++ string interpolation/assets/js/app.js
@@ -0,0 +1,32 @@
+
+var fname="shashikant";
+var lname="kadam";
+var nativeplace="parbhani";
+var degree="engginnering";
+var branch="mechanical";
+var university="srtmu nanded";
+
+// hello sir i'm kadam shashikant from parbhani. I have completed mechanical enginnering from srtmu nanded.
+
+var result1= "Hello sir I'm "+lname +" " + fname + " from " + nativeplace + ". I have completed " +  branch +" " + degree + " from " + university +"."
+
+console.log(result1);
+document.write(result1);
+
+var fname="virat";
+var lname="kohli";
+var nation="indian"
+var speciality="cricketer";
+var dob="5 nov 1988";
+var height="5 ft 9 inch";
+var nname="cheeku";
+var father="prem kohli";
+var mother="saroj kohli";
+var about="right-handed batsman";
+var awards="arjuna award, padma shri and the rajiv gandhi khel ratna award";
+
+var result2=  fname +" "+ lname + " is an "+ nation +" "+speciality+ " he was born on "+ dob +"." +" his  nickname is "+ nname +"."+
+				" his father's name is "+   fname +" his mother name is " + mother + " he is a " + about +"." + " he was given the " + awards +"."
+				
+console.log(result2);
+document.write(result2);
